e61399162ad309a9167c79c260ba4534bea67740
Mawchin <memegaper200@gmail.com>
2026-05-08 13:16:42 +0300

    Modify the logic file and fix bugs

--- Linear_Coding.m
+++ Linear_Coding.m
@@ -1,35 +1,35 @@
-function varargout = encode(varargin)
-% ENCODE MATLAB code for encode.fig
-%      ENCODE, by itself, creates a new ENCODE or raises the existing
+function varargout = Linear_Coding(varargin)
+% LINEAR_CODING MATLAB code for Linear_Coding.fig
+%      LINEAR_CODING, by itself, creates a new LINEAR_CODING or raises the existing
 %      singleton*.
 %
-%      H = ENCODE returns the handle to a new ENCODE or the handle to
+%      H = LINEAR_CODING returns the handle to a new LINEAR_CODING or the handle to
 %      the existing singleton*.
 %
-%      ENCODE('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in ENCODE.M with the given input arguments.
+%      LINEAR_CODING('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in LINEAR_CODING.M with the given input arguments.
 %
-%      ENCODE('Property','Value',...) creates a new ENCODE or raises the
+%      LINEAR_CODING('Property','Value',...) creates a new LINEAR_CODING or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before encode_OpeningFcn gets called.  An
+%      applied to the GUI before Linear_Coding_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to encode_OpeningFcn via varargin.
+%      stop.  All inputs are passed to Linear_Coding_OpeningFcn via varargin.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help encode
+% Edit the above text to modify the response to help Linear_Coding
 
-% Last Modified by GUIDE v2.5 07-May-2026 02:21:28
+% Last Modified by GUIDE v2.5 08-May-2026 11:20:27
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @encode_OpeningFcn, ...
-                   'gui_OutputFcn',  @encode_OutputFcn, ...
+                   'gui_OpeningFcn', @Linear_Coding_OpeningFcn, ...
+                   'gui_OutputFcn',  @Linear_Coding_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -44,26 +44,26 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before encode is made visible.
-function encode_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before Linear_Coding is made visible.
+function Linear_Coding_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to encode (see VARARGIN)
+% varargin   command line arguments to Linear_Coding (see VARARGIN)
 
-% Choose default command line output for encode
+% Choose default command line output for Linear_Coding
 handles.output = hObject;
 
 % Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes encode wait for user response (see UIRESUME)
+% UIWAIT makes Linear_Coding wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = encode_OutputFcn(hObject, eventdata, handles) 
+function varargout = Linear_Coding_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -83,14 +83,10 @@ function message_u_encoding_Callback(hObject, eventdata, handles)
 %        str2double(get(hObject,'String')) returns contents of message_u_encoding as a double
 
 u = str2num(get(hObject,'String'));
-
-if isempty(u)
-    errordlg('Please enter original message');
-    return;
+if ~isempty(u)
+    handles.u = u(:)';
+    guidata(hObject, handles);
 end
-
-handles.u = u(:)';
-guidata(hObject, handles); 
 
 
 
@@ -117,19 +113,10 @@ function pre_matrix_Callback(hObject, eventdata, handles)
 %        str2double(get(hObject,'String')) returns contents of pre_matrix as a double
 
 p = str2num(get(hObject,'String'));
-
-if isempty(p)
-    errordlg('Please enter predefined matrix');
-    return;
+if ~isempty(p) && ~any(p(:) ~= 0 & p(:) ~= 1)
+    handles.p = p;
+    guidata(hObject, handles);
 end
-
-if any(p(:) ~= 0 & p(:) ~= 1)
-    errordlg('P must be binary matrix (0/1)');
-    return;
-end
-
-handles.p = p;
-guidata(hObject, handles);
 
 
 
@@ -155,15 +142,11 @@ function n_encode_Callback(hObject, eventdata, handles)
 % Hints: get(hObject,'String') returns contents of n_encode as text
 %        str2double(get(hObject,'String')) returns contents of n_encode as a double
 
-n = str2double(get(hObject,'String')); 
-
-if isnan(n) || isempty(n)
-    errordlg('Please enter the value of n');
-    return;
+n = str2double(get(hObject,'String'));
+if ~isnan(n)
+    handles.n = n;
+    guidata(hObject, handles);
 end
-
-handles.n = n;
-guidata(hObject, handles);
 
 
 % --- Executes during object creation, after setting all properties.
@@ -188,15 +171,11 @@ function k_encode_Callback(hObject, eventdata, handles)
 % Hints: get(hObject,'String') returns contents of k_encode as text
 %        str2double(get(hObject,'String')) returns contents of k_encode as a double
 
-k = str2double(get(hObject,'String')); 
-
-if isnan(k) || isempty(k)
-    errordlg('Please enter the value of k');
-    return;
+k = str2double(get(hObject,'String'));
+if ~isnan(k)
+    handles.k = k;
+    guidata(hObject, handles);
 end
-
-handles.k = k;
-guidata(hObject, handles);
 
 
 
@@ -223,14 +202,10 @@ function message_v_decoding_Callback(hObject, eventdata, handles)
 %        str2double(get(hObject,'String')) returns contents of message_v_decoding as a double
 
 v = str2num(get(hObject,'String'));
-
-if isempty(v)
-    errordlg('Please enter decoded message');
-    return;
+if ~isempty(v)
+    handles.v = v(:)';
+    guidata(hObject, handles);
 end
-
-handles.v = v; 
-guidata(hObject, handles); 
 
 
 
@@ -257,15 +232,10 @@ function generator_Callback(hObject, eventdata, handles)
 %        str2double(get(hObject,'String')) returns contents of generator as a double
 
 g = str2num(get(hObject,'String'));
-
-if isempty(g)
-    errordlg('Please enter generator matrix');
-    return;
+if ~isempty(g)
+    handles.g = g;
+    guidata(hObject, handles);
 end
-
-handles.g = g; 
-guidata(hObject, handles);
-
 
 
 % --- Executes during object creation, after setting all properties.
@@ -290,16 +260,11 @@ function n_decode_Callback(hObject, eventdata, handles)
 % Hints: get(hObject,'String') returns contents of n_decode as text
 %        str2double(get(hObject,'String')) returns contents of n_decode as a double
 
-n = str2double(get(hObject,'String')); 
-
-if isnan(n) || isempty(n)
-    errordlg('Please enter the value of n');
-    return;
+n = str2double(get(hObject,'String'));
+if ~isnan(n)
+    handles.n_dec = n;
+    guidata(hObject, handles);
 end
-
-
-handles.n = n;
-guidata(hObject, handles);
 
 
 
@@ -325,15 +290,11 @@ function k_decode_Callback(hObject, eventdata, handles)
 % Hints: get(hObject,'String') returns contents of k_decode as text
 %        str2double(get(hObject,'String')) returns contents of k_decode as a double
 
-k = str2double(get(hObject,'String')); 
-
-if isnan(k) || isempty(k)
-    errordlg('Please enter the value of k');
-    return;
+k = str2double(get(hObject,'String'));
+if ~isnan(k)
+    handles.k_dec = k;
+    guidata(hObject, handles);
 end
-
-handles.k = k;
-guidata(hObject, handles);
 
 
 
@@ -359,39 +320,6 @@ function v_encoded_output_Callback(hObject, eventdata, handles)
 % Hints: get(hObject,'String') returns contents of v_encoded_output as text
 %        str2double(get(hObject,'String')) returns contents of v_encoded_output as a double
 
-if ~isfield(handles,'u') || ~isfield(handles,'p') || ~isfield(handles,'k') || ~isfield(handles,'n')
-    errordlg('Missing inputs');
-    return;
-end
-
-n = handles.n;
-k = handles.k;
-p = handles.p;
-u = handles.u;
-
-if length(u) ~= k
-    errordlg('u must have length k');
-    return;
-end
-
-if size(p,2) ~= n-k || size(p,1) ~= k
-    errordlg('P matrix size must be k × (n-k)');
-    return;
-end
-
-g = [p eye(k)];
-
-if size(g,2) ~= n
-    errordlg('Invalid G matrix size');
-    return;
-end
-
-v = mod(u * g , 2);
-
-handles.v_encoded = v;
-guidata(hObject, handles);
-set(handles.v_encoded_output,'String', mat2str(handles.v_encoded));
-
 
 % --- Executes during object creation, after setting all properties.
 function v_encoded_output_CreateFcn(hObject, eventdata, handles)
@@ -414,38 +342,6 @@ function check_for_errors_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of check_for_errors as text
 %        str2double(get(hObject,'String')) returns contents of check_for_errors as a double
-
-if ~isfield(handles,'v_fixed') || ~isfield(handles,'g') || ~isfield(handles,'k') || ~isfield(handles,'n') || ~isfield(handles,'p')
-    errordlg('Missing inputs');
-    return;
-end
-
-g = handles.g;
-v = handles.v_fixed;
-k = handles.k;
-n = handles.n;
-
-v = v(:)';
-
-x = n - k;
-
-P = g(:, 1:x);
-H = [eye(x) P'];
-
-
-syndrome_vector = mod(v * H', 2);
-
-if sum(syndrome_vector) == 0
-    set(handles.check_for_errors,'String','No errors in message');
-else
-    set(handles.check_for_errors,'String','Errors detected');
-end
-
-handles.syndrome = syndrome_vector;
-handles.v_fixed = v;
-handles.x = x;
-
-guidata(hObject, handles);
 
 
 
@@ -471,23 +367,6 @@ function original_u_Callback(hObject, eventdata, handles)
 % Hints: get(hObject,'String') returns contents of original_u as text
 %        str2double(get(hObject,'String')) returns contents of original_u as a double
 
-if ~isfield(handles,'v_fixed')
-    errordlg('No decoded message yet');
-    return;
-end
-
-if ~isfield(handles,'x') || ~isfield(handles,'n')
-    errordlg('Missing inputs');
-    return;
-end
-
-v = handles.v_fixed;
-x = handles.x;
-n = length(v);
-
-u = v(x+1:n);
-
-set(hObject,'String', mat2str(u));
 
 
 
@@ -513,38 +392,6 @@ function correct_v_decoded_Callback(hObject, eventdata, handles)
 % Hints: get(hObject,'String') returns contents of correct_v_decoded as text
 %        str2double(get(hObject,'String')) returns contents of correct_v_decoded as a double
 
-if ~isfield(handles,'g') || ~isfield(handles,'v')
-    errordlg('Please enter all inputs first');
-    return;
-end
-
-g = handles.g;
-v = handles.v;
-
-
-k = handles.k;
-n = handles.n;
-v = v(:)';
-x = n - k;
-
-P = g(:, 1:x);
-H = [eye(x) P'];
-
-syndrome_vector = mod(v * H', 2);
-if sum(syndrome_vector) ~= 0
-    for i = 1:n
-        if isequal(H(:,i)', syndrome_vector)
-            v(i) = 1 - v(i);
-            break;
-        end
-    end
-end
-
-set(hObject,'String', mat2str(v));
-
-handles.v_fixed = v;
-guidata(hObject, handles);
-
 
 
 % --- Executes during object creation, after setting all properties.
@@ -567,7 +414,7 @@ function Run_Encode_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 if ~isfield(handles,'u') || ~isfield(handles,'p') || ~isfield(handles,'k') || ~isfield(handles,'n')
-    errordlg('Missing encode inputs');
+    errordlg('Missing inputs');
     return;
 end
 
@@ -576,9 +423,15 @@ p = handles.p;
 k = handles.k;
 n = handles.n;
 
+if k >= n
+    errordlg('n must be greater than k');
+    return;
+end
+
 g = [p eye(k)];
 v = mod(u * g, 2);
 
+handles.g = g;
 handles.v_encoded = v;
 handles.v_fixed = v;
 
@@ -604,12 +457,28 @@ g = str2num(get(handles.generator, 'String'));
 k = str2double(get(handles.k_decode, 'String'));
 n = str2double(get(handles.n_decode, 'String'));
 
+if k >= n
+    errordlg('n must be greater than k');
+    return;
+end
+
+if size(g,1) ~= k || size(g,2) ~= n
+    errordlg('G matrix must be k × n');
+    return;
+end
+
+
 if isempty(g) || isnan(k) || isnan(n)
-    errordlg('Please enter G, n, and k for decoding');
+    errordlg('Please enter g, n, and k for decoding');
     return;
 end
 
 v = v(:)';
+if length(v) ~= n
+    errordlg('Length of v must equal n');
+    return;
+end
+
 x = n - k;
 P = g(:, 1:x);
 H = [eye(x) P'];
@@ -629,8 +498,8 @@ end
 
 handles.v_fixed = v_fixed;
 handles.x = x;
-handles.k = k;
-handles.n = n;
+handles.k_dec = k;
+handles.n_dec = n;
 handles.g = g;
 
 if sum(syndrome) == 0
